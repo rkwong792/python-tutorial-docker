b2779ec53d955d13881656b6eccdffd85997a652
Richard Kwong <rkwong792@gmail.com>
2021-09-05 00:23:16 -0400

    Python tutorial with Docker

---
+++ python/introduction/src/app.py
@@ -0,0 +1,27 @@
+# Python is a scripting language.
+
+class Customer:
+    # constructor
+    def __init__(self, c="", f="", l=""):
+        self.customerID = c
+        self.firstName = f
+        self.lastName = l
+
+    # self = the actual object itself that the class represents
+    # represents the instance of the class
+    def fullName(self):
+        return self.firstName + " " + self.lastName
+
+def getCustomers():
+    customers = {
+        "a": Customer("a","James","Baker"),
+        "b": Customer("b","Jonathan","D"),
+        "c": Customer("c","Aleem","Janmohamed"),
+        "d": Customer("d","Ivo","Galic")
+    }
+    return customers
+
+customers = getCustomers()
+
+for customerID in customers:
+    print(customers[customerID].fullName())
